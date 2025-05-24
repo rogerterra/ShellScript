48425a27ad88030b94aca782c5f5e1cb2a2499ef
rogerterra <93686433+rogerterra@users.noreply.github.com>
2025-05-24 20:06:36 -0300

    Create debug_1.sh

---
+++ mod4/debug_1.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# listaUsuarios.sh - Extrai usuários do /etc/passwd
+
+# Nome projeto: listaUsuarios.sh
+# Autor: ROG
+# Manutenção: ROG
+
+# -------------------------------------------------------------#
+
+# Irá extrair usuários do /etc/passwd, havendo a possibilidade de
+# colocar em maiúsculo e em ordem alfabética
+
+# Exemplos:
+#       $ ./listaUsuarios.sh -s -m
+#       Neste exemplo ficará em maiúsculo e em ordem alfabética
+
+# -------------------------------------------------------------#
+
+# Testado em:
+#  bash 5.2.21S
+
+# -------------------------------------------------------------#
+# VARIÁVEIS
+
+USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
+
+MENSAGEM_USO="
+  $(basename $0) - [OPÇÕES]
+
+    -h - Menu de ajuda
+    -v - Versão
+    -s - Ordernar a saída
+    -m - Coloca em maiúsculo
+"
+VERSAO="v1.3"
+CHAVE_ORDENA=0
+CHAVE_MAISCULO=0
+
+# -------------------------------------------------------------#
+# EXECUÇÃO
+
+while test -n "$1"
+do
+  case "$1" in
+    -h) echo "$MENSAGEM_USO" && exit 0                 ;;
+    -v) echo "$VERSAO" && exit 0                       ;;
+    -s) CHAVE_ORDENA=1                                 ;;
+    -m) CHAVE_MAISCULO=1                               ;;
+     *) echo "Opção inválida, avalie o -h." && exit 1  ;;
+  esac
+  shift
+done
+
+set -x
+[ $CHAVE_ORDENA -eq 1 ] && USUARIOS=$(echo "$USUARIOS" | sort)
+[ $CHAVE_MAISCULO -eq 1 ] && USUARIOS=$(echo "$USUARIOS" | tr [a-z] [A-Z])
+
+echo "$USUARIOS"
+set +x
+
+# -------------------------------------------------------------#
